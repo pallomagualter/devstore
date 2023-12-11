27867eaa1707f4f47ff3e4686da8fc9de4dd4085
Palloma <pallomakcgualter@gmail.com>
2023-12-11 12:00:28 -0300

    feat: using cache

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -31,8 +31,13 @@ export async function generateMetadata({
   }
 }
 
-export function generateStaticParams() {
-  return [{ slug: 'moletom-never-stop-learning' }]
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
 }
 
 export default async function ProductPage({ params }: ProductProps) {
