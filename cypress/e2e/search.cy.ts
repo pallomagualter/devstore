2214e30a59561a36806e3e32a138e0519d94ab4d
Palloma <pallomakcgualter@gmail.com>
2023-12-13 11:32:50 -0300

    feat: including tests e2e

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -8,6 +8,19 @@ describe('search products', () => {
     cy.get('a[href^="/product"]').should('exist')
   })
 
+  it('should be able to search for products and add it to the cart', () => {
+    cy.searchByQuery('java')
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=java')
+
+    cy.get('a[href^="/product"]').should('exist')
+    cy.contains('Java').click({ multiple: true })
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
   it('should not be able to search page without a search query', () => {
     cy.on('uncaught:exception', () => {
       return false
